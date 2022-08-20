e34d04a63ee9e50904c0a62607c1d299b7f86653
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2022-08-20 02:17:49 -0300

    adjust alias in pokemon api

--- src/pages/api/pokemon.ts
+++ src/pages/api/pokemon.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import type { ErrorType, PokemonResponseType } from '../../types'
+import type { ErrorType, PokemonResponseType } from '@types'
 import axios from 'axios'
 import { ApiUrlsEnum } from '@enums'
 
