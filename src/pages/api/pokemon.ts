dd1b0f8ed01f148e04851520ae592d824c980ebb
Leonardo Marcos <marcosleonardosss@gmail.com>
2022-10-14 09:23:33 -0300

    adjust User-Agent in pokemon api

--- src/pages/api/pokemon.ts
+++ src/pages/api/pokemon.ts
@@ -3,6 +3,10 @@ import axios from 'axios'
 
 import type { ErrorType, PokemonResponseType } from '@types'
 import { ApiUrlsEnum, StatusCodeEnum } from '@enums'
+import packageInfo from '../../../package.json'
+
+const PROJECT_URL = 'https://pokemon-evolution.vercel.app'
+const PROJECT_NAME = 'Pokemon Evolution'
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,17 +17,18 @@ export default async function handler(
       baseURL: ApiUrlsEnum.POKEMON_API,
     })
 
-    const { data } = await http.get<PokemonResponseType>(`/${req.query.id}`, {
+    const { data } = await http.get<PokemonResponseType[]>(`/${req.query.id}`, {
       headers: {
-        'User-Agent':
-          'Pokemon Evolution (https://pokemon-evolution.vercel.app/, 1.0.0)',
+        'User-Agent': `${PROJECT_NAME} (${PROJECT_URL}, ${packageInfo?.version})`,
       },
     })
 
-    const id = String(req.query.id).padStart(3, '0')
-    const sprite = `https://epackoug.sirv.com/pokemon/${id}.png`
+    const [pokemon] = data
 
-    res.status(StatusCodeEnum.SUCCESS).json({ ...data[0], sprite })
+    const id = String(req.query.id).padStart(3, '0')
+    const sprite = `${process.env.NEXT_PUBLIC_POKEMON_IMAGES}/${id}.png`
+
+    res.status(StatusCodeEnum.SUCCESS).json({ ...pokemon, sprite })
   } catch (error) {
     const errorApi = error as ErrorType
 
