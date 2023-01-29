5d2c752010ffe259be704ae789f34ee8c6dce6cf
Leonardo Marcos <marcosleonardosss@gmail.com>
2023-01-29 00:24:11 -0300

    remove cypress from project

--- cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
-  e2e: {
-    experimentalStudio: true,
-    baseUrl: 'https://pokemon-evolution.vercel.app',
-    // baseUrl: 'http://localhost:3000',
-  },
-})
